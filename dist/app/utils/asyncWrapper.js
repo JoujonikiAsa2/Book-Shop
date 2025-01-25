92f374a1ff08bdb0e20726fdbdb23d2794a92af4
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-01-26 00:09:04 +0600

    added error handler and async wrapper

--- dist/app/utils/asyncWrapper.js
+++ dist/app/utils/asyncWrapper.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.asyncWrapper = void 0;
+//try catch handler
 const asyncWrapper = (fn) => {
     return (req, res, next) => {
         Promise.resolve(fn(req, res, next)).catch((error) => next(error));
